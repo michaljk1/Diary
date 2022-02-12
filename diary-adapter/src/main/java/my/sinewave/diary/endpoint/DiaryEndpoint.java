769a62c38f81010c00b7a90ec86a34125c01d5dc
michaljk1 <theNeweSTls1>
2022-02-12 21:08:28 +0100

    optimize imports

--- diary-adapter/src/main/java/my/sinewave/diary/endpoint/DiaryEndpoint.java
+++ diary-adapter/src/main/java/my/sinewave/diary/endpoint/DiaryEndpoint.java
@@ -3,7 +3,11 @@ package my.sinewave.diary.endpoint;
 import lombok.RequiredArgsConstructor;
 import my.sinewave.diary.DiaryRatings;
 import my.sinewave.diary.service.DiaryService;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
 
